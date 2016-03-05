97d4d5a7047460ba19cf72668ec4225ca10b4f94
PentaHiggs <Ichiroska89@yahoo.com>
2016-03-05 00:43:01 -0500

    Added Q function update routine and policy action selection.  Visually complete, but not yet tested!

--- Project4/smartcab/agent.py
+++ Project4/smartcab/agent.py
@@ -2,6 +2,8 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+from python import namedtuple
+
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -11,31 +13,82 @@ class LearningAgent(Agent):
         print "LearningAgent.__init__ run"
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        
         # TODO: Initialize any additional variables here
-        self.destination = None
+        
         self.actions = [None, 'forward', 'left', 'right']
+        self.stateTuple = namedtuple('stateTuple', 
+                        ['light','oncoming','right','left','next_waypoint','deadline_approaching'])
+        self.stateActionTuple = namedtuple('stateActionTuple',
+                        ['state', 'action'])
+                        
+        self.Q = {}
+        self.s_a = None
+        self.reward = None
         
     def reset(self, destination=None):
         """ Resets the LearningAgent.  Also called after __init__ by the environment on start """
         self.planner.route_to(destination)
-        self.destination = destination
-
+        self.s_a = None
+        self.reward = None
+        
     def update(self, t):
+        # Q function learning parameters
+        defaultVal = 10.
+        epsilon = .1
+        learningRate = 1./t
+        discountFactor = 1./2.
+        
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
         
         # TODO: Update state
+        state = stateTuple( 'light'                 = inputs['light'],
+                            'oncoming'              = inputs['oncoming'],
+                            'right'                 = inputs['right'],
+                            'left'                  = inputs['left'],
+                            'next_waypoint'         = self.next_waypoint,
+                            'deadline_approaching'  = (deadline < 5),
+                            )
         
+   
         # TODO: Select action according to your policy
-        action = self.actions[random.randint(0,3)]
-
+        action = None
+        
+        # Calculate optimal action given current knowledge of Q function
+        optimalAction = None
+        qVal = [10., 10., 10., 10.]
+        for i, action in enumerate(self.actions):
+            s_a = self.stateActionTuple(state, action)
+            if self.Q.has_key(s_a):
+                qVal[i] = self.Q[s_a]
+            else:
+                self.Q[s_a] = defaultVal
+        maxVal = max(qVal)
+        optimalAction = self.actions[ random.choice([for i,j in enumerate(qVal) if j==maxVal]) ]
+        
+        # Choose whether to act optimally or act randomly
+        if random.random() <= epsilon:
+            action = self.actions[random.randint(0,3)]
+        else:
+            action = optimalAction
+            
         # Execute action and get reward
         reward = self.env.act(self, action)
-
-        # TODO: Learn policy based on state, action, reward
-
+        
+        # Update Q function
+        optimal_s_a = self.stateActionTuple(state, optimalAction)
+        if not self.s_a == None: # if this isn't the first time we run update(self, t)
+            self.Q[s_a] = (1-learningRate) * self.Q[s_a] + \
+                learningRate * (self.reward + discountFactor * (self.Q[optimal_s_a]) 
+                
+        # Update the old state variables
+        self.s_a = s_a
+        self.reward = reward
+        
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}, waypoint =  {}".format(deadline, inputs, action, reward, self.next_waypoint)  # [debug]
 
 
